80489c1431b199a6a3766d253255c8fc49e97048
Uhucream <aborog1324@yahoo.co.jp>
2023-04-05 20:08:07 +0900

    feat: フッターのリンクを含んだコンポーネント NavigationLinks を作成

---
+++ nextjs/src/components/navigation_links.tsx
@@ -0,0 +1,270 @@
+import NextLink from "next/link";
+import { Box, Container, HStack, List, ListItem, Link } from "@chakra-ui/react";
+import { withRouter } from "next/router";
+
+export const NavigationLinks = withRouter(({ router }) => (
+  <Box as="nav">
+    <List
+      display="flex"
+      flexFlow="row nowrap"
+      justifyContent="space-between"
+      alignItems="baseline"
+      py="24px"
+      gap="44px"
+    >
+      <ListItem>
+        <Link
+          as={NextLink}
+          href="/"
+          fontSize={router.pathname === "/" ? 40 : 18}
+          fontWeight="700"
+          textTransform="uppercase"
+          color={router.pathname === "/" ? "white" : "rgba(1, 104, 183, 0.7)"}
+          _hover={{
+            textDecoration: "none",
+          }}
+        >
+          Top
+        </Link>
+      </ListItem>
+
+      <ListItem>
+        {router.pathname === "/" ? (
+          <Link
+            as={NextLink}
+            href="/news"
+            fontSize={18}
+            fontWeight="700"
+            textTransform="uppercase"
+            color={"rgba(255, 255, 255, 0.7)"}
+            _hover={{
+              textDecoration: "none",
+            }}
+          >
+            News
+          </Link>
+        ) : (
+          <Link
+            as={NextLink}
+            href="/news"
+            fontSize={router.pathname === "/news" ? 40 : 18}
+            fontWeight="700"
+            textTransform="uppercase"
+            color={
+              router.pathname === "/news" ? "#0168B7" : "rgba(1, 104, 183, 0.7)"
+            }
+            _hover={{
+              textDecoration: "none",
+            }}
+          >
+            News
+          </Link>
+        )}
+      </ListItem>
+
+      <ListItem>
+        {router.pathname === "/" ? (
+          <Link
+            as={NextLink}
+            href="/self_introduction"
+            fontSize={18}
+            fontWeight="700"
+            textTransform="uppercase"
+            color={"rgba(255, 255, 255, 0.7)"}
+            _hover={{
+              textDecoration: "none",
+            }}
+          >
+            自己紹介
+          </Link>
+        ) : (
+          <Link
+            as={NextLink}
+            href="/self_introduction"
+            fontSize={router.pathname === "/self_introduction" ? 40 : 18}
+            fontWeight="700"
+            textTransform="uppercase"
+            color={
+              router.pathname === "/self_introduction" ? "#0168B7" : "rgba(1, 104, 183, 0.7)"
+            }
+            _hover={{
+              textDecoration: "none",
+            }}
+          >
+            自己紹介
+          </Link>
+        )}
+      </ListItem>
+
+      <ListItem>
+        {router.pathname === "/" ? (
+          <Link
+            as={NextLink}
+            href="/projects"
+            fontSize={18}
+            fontWeight="700"
+            textTransform="uppercase"
+            color={"rgba(255, 255, 255, 0.7)"}
+            _hover={{
+              textDecoration: "none",
+            }}
+          >
+            研究紹介
+          </Link>
+        ) : (
+          <Link
+            as={NextLink}
+            href="/projects"
+            fontSize={router.pathname === "/projects" ? 40 : 18}
+            fontWeight="700"
+            textTransform="uppercase"
+            color={
+              router.pathname === "/projects" ? "#0168B7" : "rgba(1, 104, 183, 0.7)"
+            }
+            _hover={{
+              textDecoration: "none",
+            }}
+          >
+            研究紹介
+          </Link>
+        )}
+      </ListItem>
+
+      <ListItem>
+        {router.pathname === "/" ? (
+          <Link
+            as={NextLink}
+            href="/members"
+            fontSize={18}
+            fontWeight="700"
+            textTransform="uppercase"
+            color={"rgba(255, 255, 255, 0.7)"}
+            _hover={{
+              textDecoration: "none",
+            }}
+          >
+            メンバー
+          </Link>
+        ) : (
+          <Link
+            as={NextLink}
+            href="/members"
+            fontSize={router.pathname === "/members" ? 40 : 18}
+            fontWeight="700"
+            textTransform="uppercase"
+            color={
+              router.pathname === "/members" ? "#0168B7" : "rgba(1, 104, 183, 0.7)"
+            }
+            _hover={{
+              textDecoration: "none",
+            }}
+          >
+            メンバー
+          </Link>
+        )}
+      </ListItem>
+
+      <ListItem>
+        {router.pathname === "/" ? (
+          <Link
+            as={NextLink}
+            href="/publications"
+            fontSize={18}
+            fontWeight="700"
+            textTransform="uppercase"
+            color={"rgba(255, 255, 255, 0.7)"}
+            _hover={{
+              textDecoration: "none",
+            }}
+          >
+            論文
+          </Link>
+        ) : (
+          <Link
+            as={NextLink}
+            href="/publications"
+            fontSize={router.pathname === "/publications" ? 40 : 18}
+            fontWeight="700"
+            textTransform="uppercase"
+            color={
+              router.pathname === "/publications" ? "#0168B7" : "rgba(1, 104, 183, 0.7)"
+            }
+            _hover={{
+              textDecoration: "none",
+            }}
+          >
+            論文
+          </Link>
+        )}
+      </ListItem>
+
+      <ListItem>
+        {router.pathname === "/" ? (
+          <Link
+            as={NextLink}
+            href="/awards"
+            fontSize={18}
+            fontWeight="700"
+            textTransform="uppercase"
+            color={"rgba(255, 255, 255, 0.7)"}
+            _hover={{
+              textDecoration: "none",
+            }}
+          >
+            受賞履歴
+          </Link>
+        ) : (
+          <Link
+            as={NextLink}
+            href="/awards"
+            fontSize={router.pathname === "/awards" ? 40 : 18}
+            fontWeight="700"
+            textTransform="uppercase"
+            color={
+              router.pathname === "/awards" ? "#0168B7" : "rgba(1, 104, 183, 0.7)"
+            }
+            _hover={{
+              textDecoration: "none",
+            }}
+          >
+            受賞履歴
+          </Link>
+        )}
+      </ListItem>
+
+      <ListItem>
+        {router.pathname === "/" ? (
+          <Link
+            as={NextLink}
+            href="/access"
+            fontSize={18}
+            fontWeight="700"
+            textTransform="uppercase"
+            color={"rgba(255, 255, 255, 0.7)"}
+            _hover={{
+              textDecoration: "none",
+            }}
+          >
+            アクセス
+          </Link>
+        ) : (
+          <Link
+            as={NextLink}
+            href="/access"
+            fontSize={router.pathname === "/access" ? 40 : 18}
+            fontWeight="700"
+            textTransform="uppercase"
+            color={
+              router.pathname === "/access" ? "#0168B7" : "rgba(1, 104, 183, 0.7)"
+            }
+            _hover={{
+              textDecoration: "none",
+            }}
+          >
+            アクセス
+          </Link>
+        )}
+      </ListItem>
+    </List>
+  </Box>
+));
