da01034da6686c23f4fdb62aed285e514b3c65cf
Uhucream <aborog1324@yahoo.co.jp>
2023-04-05 20:29:25 +0900

    feat: 各ページ共通の内容の Layout 、 RootLayout 作成

---
+++ nextjs/src/pages/root_layout.tsx
@@ -0,0 +1,51 @@
+import { ReactElement, useEffect } from "react";
+import { Box, Container, HStack } from "@chakra-ui/react";
+import { NavigationLinks } from "../components/navigation_links";
+import { SiteTitle } from "@/components/site_title";
+
+type RootLayoutProps = Required<{
+  readonly children: ReactElement;
+}>;
+
+export const RootLayout = ({ children }: RootLayoutProps) => (
+  <>
+    <Box
+      as="header"
+      position="fixed"
+      top={0}
+      left={0}
+      right={0}
+      zIndex={100}
+      css={{
+        "&::-webkit-scrollbar": {
+          display: "none",
+        },
+      }}
+    >
+      <SiteTitle />
+    </Box>
+
+    <main>{children}</main>
+
+    <Container
+      as="footer"
+      position="fixed"
+      left={0}
+      right={0}
+      bottom={0}
+      overflowX="scroll"
+      overflowY="visible"
+      whiteSpace="nowrap"
+      maxW="1440px"
+      centerContent
+      zIndex={100}
+      css={{
+        "&::-webkit-scrollbar": {
+          display: "none",
+        },
+      }}
+    >
+      <NavigationLinks />
+    </Container>
+  </>
+);
