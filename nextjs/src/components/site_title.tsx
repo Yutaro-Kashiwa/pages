eb2b2578ec53d43c30db6c3de663f50622da1a16
Uhucream <aborog1324@yahoo.co.jp>
2023-04-06 19:36:10 +0900

    fix: SiteTitle のロゴにリンクを追加

--- nextjs/src/components/site_title.tsx
+++ nextjs/src/components/site_title.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react"
 import { withRouter } from "next/router"
-import { AspectRatio, Center, HStack, VStack, Text } from "@chakra-ui/react"
+import { AspectRatio, Center, HStack, VStack, Link, LinkOverlay, Text } from "@chakra-ui/react"
 import SakigakePrestoLogo from "@/images/sakigake_presto_logo.svg"
 
 export const SakigakeLogo: FC = () => (
@@ -13,9 +13,11 @@ export const SakigakeLogo: FC = () => (
 
 export const SiteTitle = withRouter(({ router }) => (
   <HStack w="100%" pl="40px" alignItems="flex-end" spacing="24px" bg="transparent">
-    <AspectRatio w={router.pathname === "/" ? "148px" : "123px"} flexShrink={1} ratio={148 / 76}>
-      <SakigakeLogo />
-    </AspectRatio>
+    <Link isExternal href="https://www.jst.go.jp/kisoken/presto/about/index.html">
+      <AspectRatio w={router.pathname === "/" ? "148px" : "123px"} flexShrink={1} ratio={148 / 76}>
+        <SakigakeLogo />
+      </AspectRatio>
+    </Link>
 
     <VStack p={0} pb="12px" m={0}>
       <Text fontWeight={400} fontSize="20px" color={router.pathname === "/" ? "#333333" : "#0168B7"}>プログラム異常動作の自動検出技術の創出プロジェクト</Text>
