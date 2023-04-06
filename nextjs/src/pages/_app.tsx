3cfed6d0b4f5ff63b319e91ac742b4c16d2c9d5e
Uhucream <aborog1324@yahoo.co.jp>
2023-04-06 20:58:55 +0900

    feat: 各ページで遷移アニメーションを動作させるため、_app.tsx に AnimationPresence を設置

--- nextjs/src/pages/_app.tsx
+++ nextjs/src/pages/_app.tsx
@@ -4,21 +4,24 @@ import { RootLayout } from "./root_layout";
 import { theme } from '@/config/theme';
 import { NextPageWithLayout } from '@/types/next_page_with_layout';
 import { ReactElement } from 'react';
+import { AnimatePresence } from 'framer-motion';
 
 export type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout
 }
 
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
+export default function App({ Component, pageProps, router }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page: ReactElement) => page)
 
   return (
     <ChakraProvider theme={theme}>
-      <RootLayout>
-        {getLayout(
-          <Component {...pageProps} />
-        )}
-      </RootLayout>
+      <AnimatePresence mode="wait" onExitComplete={() => window.scrollTo(0, 0)}>
+        <RootLayout>
+          {getLayout(
+            <Component key={router.asPath} {...pageProps} />
+          )}
+        </RootLayout>
+      </AnimatePresence>
     </ChakraProvider>
   );
 }
