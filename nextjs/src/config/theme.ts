e0934015a8f7214dac9a44714aab6fccc7c906cf
Uhucream <aborog1324@yahoo.co.jp>
2023-04-06 11:51:08 +0900

    feat: theme.ts に breakpoints 追加

--- nextjs/src/config/theme.ts
+++ nextjs/src/config/theme.ts
@@ -24,5 +24,12 @@ export const theme = extendTheme({
       1: "#D7D7D7",
       2: "#F5F5F5"
     }
+  },
+  breakpoints: {
+    sm: '30em', // 480px
+    md: '48em', // 768px
+    lg: '62em', // 992px
+    xl: '80em', // 1280px
+    '2xl': '96em', // 1536px
   }
 });
