703107231dc2e3bb790bf9016fcf40d11ebc73af
Uhucream <aborog1324@yahoo.co.jp>
2023-04-06 11:56:01 +0900

    feat: 複数の Layout を指定できるように
    参考URL: https://nextjs.org/docs/basic-features/layouts#with-typescript

--- nextjs/src/pages/_app.tsx
+++ nextjs/src/pages/_app.tsx
@@ -2,12 +2,22 @@ import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { RootLayout } from "./root_layout";
 import { theme } from '@/config/theme';
+import { NextPageWithLayout } from '@/types/next_page_with_layout';
+import { ReactElement } from 'react';
 
-export default function App({ Component, pageProps }: AppProps) {
+export type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page: ReactElement) => page)
+
   return (
     <ChakraProvider theme={theme}>
       <RootLayout>
-        <Component {...pageProps} />
+        {getLayout(
+          <Component {...pageProps} />
+        )}
       </RootLayout>
     </ChakraProvider>
   );
