5cf955abf3d69c47e4c35e338fb53df78b72572f
Uhucream <aborog1324@yahoo.co.jp>
2023-04-12 01:24:40 +0900

    feat: _document.tsx に、<head> タグ追加

--- nextjs/src/pages/_document.tsx
+++ nextjs/src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+        <meta name="robots" content="noindex" />
+      </Head>
       <body>
         <Main />
         <NextScript />
