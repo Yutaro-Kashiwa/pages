4d226d39fe30c6ec28073b019decf076eab66f5a
Uhucream <aborog1324@yahoo.co.jp>
2023-04-17 14:21:52 +0900

    fix: common_page_layout の背景の文字列を、スマホレイアウトでは表示しないように修正

--- nextjs/src/components/layouts/common_page_layout.tsx
+++ nextjs/src/components/layouts/common_page_layout.tsx
@@ -1,6 +1,6 @@
 import { ReactElement } from "react";
 import { Ubuntu } from "next/font/google";
-import { Box, HStack, Container, Text } from "@chakra-ui/react";
+import { Box, HStack, Container, Text, Hide } from "@chakra-ui/react";
 
 type CommonBackgroundLayoutProps = Required<{
   readonly children: ReactElement;
@@ -29,21 +29,23 @@ export const CommonPageLayout = ({
     backgroundAttachment="fixed"
     backgroundColor="#f5f5f5"
   >
-    <Container position="absolute" bottom="28px" right="0px" zIndex={0}>
-      <HStack justifyContent="flex-end" textAlign="right" whiteSpace="nowrap">
-        <Text
-          fontFamily={ubuntuFont.style.fontFamily}
-          fontWeight={700}
-          fontSize="180px"
-          color="#d7d7d7"
-          opacity={0.25}
-          textTransform="capitalize"
-          userSelect="none"
-        >
-          {title}
-        </Text>
-      </HStack>
-    </Container>
+    <Hide below="md">
+      <Container position="absolute" bottom="28px" right="0px" zIndex={0}>
+        <HStack justifyContent="flex-end" textAlign="right" whiteSpace="nowrap">
+          <Text
+            fontFamily={ubuntuFont.style.fontFamily}
+            fontWeight={700}
+            fontSize="180px"
+            color="#d7d7d7"
+            opacity={0.25}
+            textTransform="capitalize"
+            userSelect="none"
+          >
+            {title}
+          </Text>
+        </HStack>
+      </Container>
+    </Hide>
 
     <Box position="relative" zIndex={5}>{children}</Box>
   </Box>
