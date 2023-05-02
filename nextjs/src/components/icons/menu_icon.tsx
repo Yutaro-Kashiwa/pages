b26c67c216f7d3b2f3c858a38eb866e6db7bdbd3
Uhucream <aborog1324@yahoo.co.jp>
2023-05-02 11:14:09 +0900

    feat: スマホレイアウト用のドロワーの作成と設置

---
+++ nextjs/src/components/icons/menu_icon.tsx
@@ -0,0 +1,13 @@
+import { createIcon } from "@chakra-ui/react";
+
+export const MenuIcon = createIcon({
+  displayName: 'MenuIcon',
+  viewBox: '0 0 23 14',
+  path: (
+    <>
+      <path d="M23 2H0V0H23V2Z" />
+      <path d="M23 8H0V6H23V8Z" />
+      <path d="M23 14H0V12H23V14Z" />
+    </>
+  )
+})
