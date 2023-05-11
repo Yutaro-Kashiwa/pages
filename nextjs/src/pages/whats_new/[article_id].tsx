c0e10598f6097c778f0a85dd6058984cc706a690
Uhucream <aborog1324@yahoo.co.jp>
2023-05-11 09:45:35 +0900

    feat: WhatsNewDetailPage のレスポンシブ対応

--- nextjs/src/pages/whats_new/[article_id].tsx
+++ nextjs/src/pages/whats_new/[article_id].tsx
@@ -12,6 +12,7 @@ import {
   Image,
   Link,
   Show,
+  SimpleGrid,
   Text,
   VStack,
   chakra,
@@ -64,105 +65,157 @@ export const WhatsNewDetailPage: NextPageWithLayout<PageProps> = ({
   const shouldShowSlidingExitAnimation = asPath === "/whats_new"
 
   return (
-    <motion.div
-      initial={refererPath === "/whats_new" ? { y: "100vh" } : { opacity: 0 }}
-      animate={refererPath === "/whats_new" ? { y: 0 } : { opacity: 1 }}
-      exit={shouldShowSlidingExitAnimation ? { y: "100vh" } : { opacity: 0 }}
-      transition={{
-        duration: 0.5,
-      }}
-      style={{
-        height: "100%"
-      }}
-    >
-      <Container maxW="1280px" h="100%" overflow="auto">
-        <VStack
-          h="100%"
-          justify="space-around"
-          alignItems="flex-start"
-        >
-          <Show above="lg">
-            <HStack position="relative" w="fit-content">
-              <Box
-                position="absolute"
-                right="-20px"
-                bottom="-16px"
-                w="199px"
-                zIndex={-1}
+    <>
+      <style global jsx>
+        {`
+          #__next {
+            overflow-x: auto;
+            overflow-y: scroll;
+          }
+        `}
+      </style>
+
+      <motion.div
+        initial={refererPath === "/whats_new" ? { y: "100vh" } : { opacity: 0 }}
+        animate={refererPath === "/whats_new" ? { y: 0 } : { opacity: 1 }}
+        exit={shouldShowSlidingExitAnimation ? { y: "100vh" } : { opacity: 0 }}
+        transition={{
+          duration: 0.5,
+        }}
+        style={{
+          height: "100%",
+        }}
+      >
+        <Container maxW="1280px" h="100%" overflow="visible">
+          <VStack h="100%" justify="space-around" alignItems="flex-start">
+            <Show above="lg">
+              <HStack position="relative" w="fit-content">
+                <Box
+                  position="absolute"
+                  right="-20px"
+                  bottom="-16px"
+                  w="199px"
+                  zIndex={-1}
+                >
+                  <AspectRatio w="100%" ratio={199 / 44}>
+                    <TitleBackgroundRect />
+                  </AspectRatio>
+                </Box>
+
+                <Heading
+                  as="h2"
+                  fontFamily={ubuntuFont.style.fontFamily}
+                  fontWeight={400}
+                  color="main"
+                  textTransform="uppercase"
+                >
+                  {`what's new`}
+                </Heading>
+              </HStack>
+            </Show>
+
+            <VStack flexBasis="60%" justify="space-between" align="flex-start">
+              <VStack
+                flexBasis="100%"
+                justify="space-evenly"
+                align="inherit"
+                spacing="48px"
               >
-                <AspectRatio w="100%" ratio={199 / 44}>
-                  <TitleBackgroundRect />
-                </AspectRatio>
-              </Box>
+                {!!createdAt && !!title && (
+                  <SimpleGrid
+                    alignItems="baseline"
+                    templateColumns="repeat(auto-fit, minmax(140px, 1fr))"
+                    templateRows="auto"
+                  >
+                    {!!createdAt && (
+                      <Text
+                        as="time"
+                        dateTime={createdAt}
+                        fontFamily={ubuntuFont.style.fontFamily}
+                        fontWeight={400}
+                        fontSize={{
+                          base: "calc(0.875rem + ((1vw - 3.75px) * 0.3756))",
+                          lg: 18,
+                        }}
+                      >
+                        {format(parseISO(createdAt), "yyyy . MM . dd")}
+                      </Text>
+                    )}
 
-              <Heading
-                as="h2"
-                fontFamily={ubuntuFont.style.fontFamily}
-                fontWeight={400}
-                color="main"
-                textTransform="uppercase"
-              >
-                {`what's new`}
-              </Heading>
-            </HStack>
-          </Show>
-
-          <VStack flexBasis="60%" justify="space-between" align="flex-start">
-            <VStack flexBasis="100%" justify="space-evenly" align="inherit" spacing="48px">
-              {!!createdAt && !!title && (
-                <HStack align="baseline" spacing="40px">
-                  {!!createdAt && (
-                    <Text
-                      as="time"
-                      dateTime={createdAt}
-                      fontFamily={ubuntuFont.style.fontFamily}
-                      fontWeight={400}
-                      fontSize={18}
-                    >
-                      {format(parseISO(createdAt), "yyyy . MM . dd")}
-                    </Text>
-                  )}
-
-                  {!!title && (
-                    <Heading as="h2" fontWeight={400} fontSize={32}>
-                      {title}
-                    </Heading>
-                  )}
-                </HStack>
-              )}
-
-              <HStack align="flex-start" spacing="36px">
-                {!!body && (
-                  <Box p={0} fontSize={16} lineHeight="1.5" whiteSpace="pre-wrap">
-                    {body}
-                  </Box>
+                    {!!title && (
+                      <Heading
+                        as="h2"
+                        fontWeight={400}
+                        fontSize={{
+                          base: "calc(1.25rem + ((1vw - 3.75px) * 1.1268))",
+                          lg: 32,
+                        }}
+                        whiteSpace="nowrap"
+                      >
+                        {title}
+                      </Heading>
+                    )}
+                  </SimpleGrid>
                 )}
 
-                <AspectRatio flexBasis="50%">
+                <Show below="lg">
                   <ChakraNextImage
+                    // @ts-ignore
+                    position="relative !important"
+                    w="100%"
+                    maxH="192px"
                     src={imageURL ?? ""}
                     alt={title ?? ""}
                     // @ts-ignore
                     fill
+                    objectFit="contain"
                   />
-                </AspectRatio>
-              </HStack>
-            </VStack>
+                </Show>
 
-            <Link
-              as={NextLink}
-              href="/whats_new"
-              pr="8px"
-              fontSize={16}
-              color="main"
-            >
-              一覧へ戻る ↑
-            </Link>
+                <HStack align="flex-start" spacing="36px">
+                  {!!body && (
+                    <Box
+                      p={0}
+                      fontSize={{
+                        base: "calc(0.75rem + ((1vw - 3.75px) * 0.3756))",
+                        lg: 16,
+                      }}
+                      lineHeight="1.5"
+                      whiteSpace="pre-wrap"
+                    >
+                      {body}
+                    </Box>
+                  )}
+
+                  <Show above="lg">
+                    <AspectRatio flexBasis="50%">
+                      <ChakraNextImage
+                        src={imageURL ?? ""}
+                        alt={title ?? ""}
+                        // @ts-ignore
+                        fill
+                      />
+                    </AspectRatio>
+                  </Show>
+                </HStack>
+              </VStack>
+
+              <Link
+                as={NextLink}
+                href="/whats_new"
+                pr="8px"
+                fontSize={16}
+                fontWeight={{ base: 700, lg: 400 }}
+                color={{ base: "#999999", lg: "main" }}
+              >
+                一覧へ戻る ↑
+              </Link>
+            </VStack>
           </VStack>
-        </VStack>
-      </Container>
-    </motion.div>
-  )
+        </Container>
+      </motion.div>
+    </>
+  );
 };
 
 WhatsNewDetailPage.getLayout = (page) => (
