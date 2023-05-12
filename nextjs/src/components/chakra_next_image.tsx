9d3a956c01c427d0c3eedee4f62029bb3e80e88c
Uhucream <aborog1324@yahoo.co.jp>
2023-05-12 09:36:53 +0900

    feat: ChakraNextImage の shouldForwardProp に、objectFit 追加

--- nextjs/src/components/chakra_next_image.tsx
+++ nextjs/src/components/chakra_next_image.tsx
@@ -2,5 +2,5 @@ import { chakra } from "@chakra-ui/react";
 import Image from "next/image";
 
 export const ChakraNextImage = chakra(Image, {
-  shouldForwardProp: (prop) => ["width", "height", "src", "alt", "fill"].includes(prop),
+  shouldForwardProp: (prop) => ["width", "height", "src", "alt", "fill", "objectFit"].includes(prop),
 })
