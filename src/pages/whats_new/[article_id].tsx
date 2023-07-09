736970804f23f1f55b647cc769a29e04b3674f16
Uhucream <aborog1324@yahoo.co.jp>
2023-07-10 00:10:55 +0900

    fix: WhatsNewDetailPage から、親の height に依存する実装を削除

--- src/pages/whats_new/[article_id].tsx
+++ src/pages/whats_new/[article_id].tsx
@@ -61,164 +61,150 @@ export const WhatsNewDetailPage: NextPageWithLayout<PageProps> = ({
   const shouldShowSlidingExitAnimation = asPath === "/whats_new";
 
   return (
-    <>
-      <style global jsx>
-        {`
-          #__next {
-            overflow-x: auto;
-            overflow-y: scroll;
-          }
-        `}
-      </style>
-
-      <motion.div
-        initial={refererPath === "/whats_new" ? { y: "100vh" } : { opacity: 0 }}
-        animate={refererPath === "/whats_new" ? { y: 0 } : { opacity: 1 }}
-        exit={shouldShowSlidingExitAnimation ? { y: "100vh" } : { opacity: 0 }}
-        transition={{
-          duration: 0.5,
-        }}
-        style={{
-          height: "100%",
-        }}
-      >
-        <Container maxW="1280px" pt="32px" pb="84px" overflow="visible">
-          <VStack justify="space-around" alignItems="flex-start" rowGap="10vh">
-            <Show above="lg">
-              <HStack position="relative" w="fit-content">
-                <Box
-                  position="absolute"
-                  right="-20px"
-                  bottom="-16px"
-                  w="199px"
-                  zIndex={-1}
-                >
-                  <AspectRatio w="100%" ratio={199 / 44}>
-                    <TitleBackgroundRect />
-                  </AspectRatio>
-                </Box>
-
-                <Heading
-                  as="h2"
-                  fontFamily={ubuntuFont.style.fontFamily}
-                  fontWeight={400}
-                  color="main"
-                  textTransform="uppercase"
-                >
-                  {`what's new`}
-                </Heading>
-              </HStack>
-            </Show>
-
-            <VStack flexBasis="60%" justify="space-between" align="flex-start">
-              <VStack
-                flexBasis="100%"
-                justify="space-evenly"
-                align="inherit"
-                spacing="48px"
+    <motion.div
+      initial={refererPath === "/whats_new" ? { y: "100vh" } : { opacity: 0 }}
+      animate={refererPath === "/whats_new" ? { y: 0 } : { opacity: 1 }}
+      exit={shouldShowSlidingExitAnimation ? { y: "100vh" } : { opacity: 0 }}
+      transition={{
+        duration: 0.5,
+      }}
+    >
+      <Container maxW="1280px" my="5vh">
+        <VStack justify="space-around" alignItems="flex-start" rowGap="72px">
+          <Show above="lg">
+            <HStack position="relative" w="fit-content">
+              <Box
+                position="absolute"
+                right="-20px"
+                bottom="-16px"
+                w="199px"
+                zIndex={-1}
               >
-                {!!createdAt && !!title && (
-                  <HStack
-                    align="baseline"
-                    wrap="wrap"
-                    rowGap="8px"
-                    columnGap="40px"
-                  >
-                    {!!createdAt && (
-                      <Text
-                        as="time"
-                        dateTime={createdAt}
-                        fontFamily={ubuntuFont.style.fontFamily}
-                        fontWeight={400}
-                        fontSize={{
-                          base: "calc(0.875rem + ((1vw - 3.75px) * 0.3756))",
-                          lg: 18,
-                        }}
-                        whiteSpace="nowrap"
-                      >
-                        {format(parseISO(createdAt), "yyyy . MM . dd")}
-                      </Text>
-                    )}
+                <AspectRatio w="100%" ratio={199 / 44}>
+                  <TitleBackgroundRect />
+                </AspectRatio>
+              </Box>
 
-                    {!!title && (
-                      <Heading
-                        as="h2"
-                        fontWeight={400}
-                        fontSize={{
-                          base: "calc(1.25rem + ((1vw - 3.75px) * 1.1268))",
-                          lg: 32,
-                        }}
-                      >
-                        {title}
-                      </Heading>
-                    )}
-                  </HStack>
-                )}
+              <Heading
+                as="h2"
+                fontFamily={ubuntuFont.style.fontFamily}
+                fontWeight={400}
+                color="main"
+                textTransform="uppercase"
+              >
+                {`what's new`}
+              </Heading>
+            </HStack>
+          </Show>
 
-                <Show below="lg">
-                  <ChakraNextImage
-                    // @ts-ignore
-                    position="relative !important"
-                    w="100%"
-                    maxH="192px"
-                    src={imageURL ?? ""}
-                    alt={title ?? ""}
-                    // @ts-ignore
-                    fill
-                    sx={{
-                      objectFit: "contain",
-                    }}
-                  />
-                </Show>
-
-                <HStack align="flex-start" spacing="36px">
-                  {!!body && (
-                    <Box
-                      p={0}
+          <VStack justify="space-between" align="flex-start" rowGap="16px">
+            <VStack
+              flexBasis="100%"
+              justify="space-evenly"
+              align="inherit"
+              spacing="48px"
+            >
+              {!!createdAt && !!title && (
+                <HStack
+                  align="baseline"
+                  wrap="wrap"
+                  rowGap="8px"
+                  columnGap="40px"
+                >
+                  {!!createdAt && (
+                    <Text
+                      as="time"
+                      dateTime={createdAt}
+                      fontFamily={ubuntuFont.style.fontFamily}
+                      fontWeight={400}
                       fontSize={{
-                        base: "calc(0.75rem + ((1vw - 3.75px) * 0.3756))",
-                        lg: 16,
+                        base: "calc(0.875rem + ((1vw - 3.75px) * 0.3756))",
+                        lg: 18,
                       }}
-                      lineHeight="1.5"
-                      whiteSpace="pre-wrap"
+                      whiteSpace="nowrap"
                     >
-                      {body}
-                    </Box>
+                      {format(parseISO(createdAt), "yyyy . MM . dd")}
+                    </Text>
                   )}
 
-                  <Show above="lg">
-                    <AspectRatio flexBasis="50%">
-                      <ChakraNextImage
-                        src={imageURL ?? ""}
-                        alt={title ?? ""}
-                        // @ts-ignore
-                        fill
-                      />
-                    </AspectRatio>
-                  </Show>
+                  {!!title && (
+                    <Heading
+                      as="h2"
+                      fontWeight={400}
+                      fontSize={{
+                        base: "calc(1.25rem + ((1vw - 3.75px) * 1.1268))",
+                        lg: 32,
+                      }}
+                    >
+                      {title}
+                    </Heading>
+                  )}
                 </HStack>
-              </VStack>
+              )}
 
-              <Link
-                as={NextLink}
-                href="/whats_new"
-                pr="8px"
-                fontSize={16}
-                fontWeight={{ base: 700, lg: 400 }}
-                color={{ base: "#999999", lg: "rgba(1, 104, 183, 1)" }}
-                _hover={{
-                  textDecoration: "none",
-                  color: "rgba(1, 104, 183, 0.5)",
-                }}
-                transition="ease-in 0.3s"
-              >
-                一覧へ戻る ↑
-              </Link>
+              <Show below="lg">
+                <ChakraNextImage
+                  // @ts-ignore
+                  position="relative !important"
+                  w="100%"
+                  maxH="192px"
+                  src={imageURL ?? ""}
+                  alt={title ?? ""}
+                  // @ts-ignore
+                  fill
+                  sx={{
+                    objectFit: "contain",
+                  }}
+                />
+              </Show>
+
+              <HStack align="flex-start" spacing="36px">
+                {!!body && (
+                  <Box
+                    p={0}
+                    fontSize={{
+                      base: "calc(0.75rem + ((1vw - 3.75px) * 0.3756))",
+                      lg: 16,
+                    }}
+                    lineHeight="1.5"
+                    whiteSpace="pre-wrap"
+                  >
+                    {body}
+                  </Box>
+                )}
+
+                <Show above="lg">
+                  <AspectRatio flexBasis="50%">
+                    <ChakraNextImage
+                      src={imageURL ?? ""}
+                      alt={title ?? ""}
+                      // @ts-ignore
+                      fill
+                    />
+                  </AspectRatio>
+                </Show>
+              </HStack>
             </VStack>
+
+            <Link
+              as={NextLink}
+              href="/whats_new"
+              pr="8px"
+              fontSize={16}
+              fontWeight={{ base: 700, lg: 400 }}
+              color={{ base: "#999999", lg: "rgba(1, 104, 183, 1)" }}
+              _hover={{
+                textDecoration: "none",
+                color: "rgba(1, 104, 183, 0.5)",
+              }}
+              transition="ease-in 0.3s"
+            >
+              一覧へ戻る ↑
+            </Link>
           </VStack>
-        </Container>
-      </motion.div>
-    </>
+        </VStack>
+      </Container>
+    </motion.div>
   );
 };
 
