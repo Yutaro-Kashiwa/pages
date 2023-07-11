10a6fd13ab4b7db1df9cb89d81bc0072fe49c14f
Uhucream <aborog1324@yahoo.co.jp>
2023-07-11 15:30:53 +0900

    chore: next_fonts.ts に、Inter フォントの宣言追加

--- src/config/next_fonts.ts
+++ src/config/next_fonts.ts
@@ -1,4 +1,4 @@
-import { Noto_Sans_JP, Ubuntu } from "next/font/google";
+import { Inter, Noto_Sans_JP, Ubuntu } from "next/font/google";
 
 export const variableNotoSansJPFont = Noto_Sans_JP({
   weight: "variable",
@@ -7,5 +7,9 @@ export const variableNotoSansJPFont = Noto_Sans_JP({
 
 export const ubuntuFont = Ubuntu({
   weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+});
+
+export const interFont = Inter({
   subsets: ["latin"],
 });
