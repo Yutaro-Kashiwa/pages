df02d4327fb736c7f406f84f0129a0d7c2cffb67
Hirai-Masaaki <hiraimasaaki12@gmail.com>
2024-03-26 13:13:31 +0900

    microcms(headless)実装

--- next.config.js
+++ next.config.js
@@ -1,17 +1,20 @@
 /** @type {import('next').NextConfig} */
 
-const withLinaria = require("next-with-linaria")
+const withLinaria = require("next-with-linaria");
 
 const nextConfig = {
+  images: {
+    domains: ["images.microcms-assets.io"],
+  },
   reactStrictMode: true,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"]
+      use: ["@svgr/webpack"],
     });
 
     return config;
-  }
-}
+  },
+};
 
-module.exports = withLinaria(nextConfig)
+module.exports = withLinaria(nextConfig);
--- src/pages/whats_new/article/[id].tsx
+++ src/pages/whats_new/article/[id].tsx
@@ -25,6 +25,9 @@ import { format, formatISO, parseISO, startOfToday } from "date-fns";
 import { ChakraNextImage } from "@/components/chakra_next_image";
 import { useRouter } from "next/router";
 import { ubuntuFont } from "@/config/next_fonts";
+import "@splidejs/react-splide/css/core";
+import { client } from "../../../../libs/client";
+import type { Article } from "../../../../types/article";
 
 type NewsData = {
   title?: string;
@@ -32,29 +35,16 @@ type NewsData = {
   // ISO8601 形式
   createdAt?: string;
   imageURL?: string;
+  article: Article;
 };
 
 type PageProps = {
   refererPath: string | null;
 } & NewsData;
 
-// API通信レスポンスの代わり
-const mockNewsData: NewsData = {
-  title: "タイトルが入ります",
-  body: `テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
-
-テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。`,
-  createdAt: "2022-11-09T00:00:00+09:00",
-  imageURL:
-    "https://1.bp.blogspot.com/-dXIT6B4KA-Y/X5OcMsqpZyI/AAAAAAABb6g/0_AtsYXLRNwGOXmbyTB_d7OqL7IWya88gCNcBGAsYHQ/s846/computer_laptop_angle1.png",
-};
-
 export const WhatsNewDetailPage: NextPageWithLayout<PageProps> = ({
   refererPath,
-  title,
-  body,
-  createdAt,
-  imageURL,
+  article,
 }) => {
   const { asPath } = useRouter();
 
@@ -69,7 +59,110 @@ export const WhatsNewDetailPage: NextPageWithLayout<PageProps> = ({
         duration: 0.5,
       }}
     >
-      <Container maxW="1280px" my="5vh">
+      <style jsx global>
+        {`
+          #editor_main h1,
+          #editor_main h2,
+          #editor_main h3,
+          #editor_main h4,
+          #editor_main h5,
+          #editor_main h6 {
+            font-weight: 500;
+          }
+
+          #editor_main h1 {
+            font-size: 200%;
+          }
+
+          #editor_main h2 {
+            font-size: 150%;
+          }
+
+          #editor_main h3 {
+            font-size: 120%;
+          }
+
+          #editor_main h4 {
+            font-size: 100%;
+          }
+
+          #editor_main h5 {
+            font-size: 80%;
+          }
+
+          #editor_main h6 {
+            font-size: 70%;
+          }
+
+          #editor_main ul {
+            list-style: disc;
+            padding-inline-start: 40px;
+          }
+
+          #editor_main ol {
+            list-style: decimal;
+            padding-inline-start: 40px;
+          }
+
+          #editor_main blockquote {
+            border-left: 4px solid #ccc;
+            padding-left: 16px;
+            margin-bottom: 5px;
+            margin-top: 5px;
+            text-align: justify;
+          }
+          #editor_main p code {
+            font-family: Source Code Pro, monospace;
+            display: inline-block;
+            font-size: 0.9em;
+            padding: 1px 5px;
+            border-radius: 2px;
+            color: #f8f8f2;
+            background-color: #23241f;
+            white-space: pre-wrap;
+          }
+          #editor_main pre {
+            font-family: Source Code Pro, monospace;
+            padding: 5px 10px;
+            border-radius: 3px;
+            margin-bottom: 5px;
+            margin-top: 5px;
+            color: #f8f8f2;
+            background-color: #23241f;
+            white-space: pre-wrap;
+            overflow: visible;
+          }
+
+          #editor_main sub {
+            vertical-align: sub;
+            font-size: smaller;
+          }
+
+          #editor_main sup {
+            vertical-align: super;
+            font-size: smaller;
+          }
+
+          #editor_main a {
+            color: -webkit-link;
+            text-decoration: underline;
+          }
+
+          #editor_main img {
+            display: inline;
+            max-width: 100%;
+            width: auto;
+          }
+
+          @media screen and (max-width: 767px) {
+            #editor_main > iframe {
+              width: 100%;
+              height: 49.5vw;
+            }
+          }
+        `}
+      </style>
+      <Container maxW="1280px" mt="5vh" mb="20vh">
         <VStack justify="space-around" alignItems="flex-start" rowGap="72px">
           <Show above="lg">
             <HStack position="relative" w="fit-content">
@@ -98,23 +191,24 @@ export const WhatsNewDetailPage: NextPageWithLayout<PageProps> = ({
           </Show>
 
           <VStack justify="space-between" align="flex-start" rowGap="16px">
+            {article.title}
             <VStack
               flexBasis="100%"
               justify="space-evenly"
               align="inherit"
               spacing="48px"
             >
-              {!!createdAt && !!title && (
+              {!!article.createdAt && !!article.title && (
                 <HStack
                   align="baseline"
                   wrap="wrap"
                   rowGap="8px"
                   columnGap="40px"
                 >
-                  {!!createdAt && (
+                  {!!article.createdAt && (
                     <Text
                       as="time"
-                      dateTime={createdAt}
+                      dateTime={article.createdAt}
                       fontFamily={ubuntuFont.style.fontFamily}
                       fontWeight={400}
                       fontSize={{
@@ -123,11 +217,11 @@ export const WhatsNewDetailPage: NextPageWithLayout<PageProps> = ({
                       }}
                       whiteSpace="nowrap"
                     >
-                      {format(parseISO(createdAt), "yyyy . MM . dd")}
+                      {format(parseISO(article.createdAt), "yyyy . MM . dd")}
                     </Text>
                   )}
 
-                  {!!title && (
+                  {!!article.title && (
                     <Heading
                       as="h2"
                       fontWeight={400}
@@ -136,7 +230,7 @@ export const WhatsNewDetailPage: NextPageWithLayout<PageProps> = ({
                         lg: 32,
                       }}
                     >
-                      {title}
+                      {article.title}
                     </Heading>
                   )}
                 </HStack>
@@ -148,8 +242,9 @@ export const WhatsNewDetailPage: NextPageWithLayout<PageProps> = ({
                   position="relative !important"
                   w="100%"
                   maxH="192px"
-                  src={imageURL ?? ""}
-                  alt={title ?? ""}
+                  // src={article.thumbnail.url ?? ""}
+                  src={"/noimage-white.jpg"}
+                  alt={article.title ?? ""}
                   // @ts-ignore
                   fill
                   sx={{
@@ -159,8 +254,9 @@ export const WhatsNewDetailPage: NextPageWithLayout<PageProps> = ({
               </Show>
 
               <HStack align="flex-start" spacing="36px">
-                {!!body && (
+                {!!article.body && (
                   <Box
+                    id="editor_main"
                     p={0}
                     fontSize={{
                       base: "calc(0.75rem + ((1vw - 3.75px) * 0.3756))",
@@ -168,16 +264,16 @@ export const WhatsNewDetailPage: NextPageWithLayout<PageProps> = ({
                     }}
                     lineHeight="1.5"
                     whiteSpace="pre-wrap"
-                  >
-                    {body}
-                  </Box>
+                    dangerouslySetInnerHTML={{ __html: article.body }}
+                  ></Box>
                 )}
 
                 <Show above="lg">
                   <AspectRatio flexBasis="50%">
                     <ChakraNextImage
-                      src={imageURL ?? ""}
-                      alt={title ?? ""}
+                      // src={article.thumbnail.url ?? ""}
+                      src={"/noimage-white.jpg"}
+                      alt={article.title ?? ""}
                       // @ts-ignore
                       fill
                     />
@@ -217,11 +313,18 @@ export const getServerSideProps: GetServerSideProps<PageProps> = async (
 ) => {
   const referer = context.req.headers.referer;
 
+  const id = context.params?.id;
+  const idExceptArray = id instanceof Array ? id[0] : id;
+  const data = await client.get({
+    endpoint: "news",
+    contentId: idExceptArray,
+  });
+
   if (!referer) {
     return {
       props: {
         refererPath: null,
-        ...mockNewsData,
+        article: data,
       },
     };
   }
@@ -232,7 +335,7 @@ export const getServerSideProps: GetServerSideProps<PageProps> = async (
   return {
     props: {
       refererPath,
-      ...mockNewsData,
+      article: data,
     },
   };
 };
